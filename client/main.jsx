c67e36cd69b627913d43ca25af3ea8ce288c99d8
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2025-08-16 19:08:53 +0530

    Update main.jsx

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,9 @@
 // React client entrypoint
-// Generated at 2025-08-16 13:21:29
+// Generated at 2025-08-16 
 import React from "react";
 import ReactDOM from "react-dom";
 
 function App(){
-  return <h1>SkillSphereAI Client Running - 2025-08-16 13:21:29</h1>
+  return <h1>SkillSphereAI Client Running - 2025-08-16</h1>
 }
 ReactDOM.render(<App/>, document.getElementById("root"));
