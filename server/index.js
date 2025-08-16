a16bb9a01ed2450a85425cf04305bd9426cfa0f3
manikanta <155448771+manikantaoruganti@users.noreply.github.com>
2025-08-16 19:08:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 // Express server entrypoint
-// Generated at 2025-08-16 13:21:29
+// Generated at 2025-08-16
 const express = require("express");
 const app = express();
-app.get("/", (req,res)=>res.send("SkillSphereAI Server Running - 2025-08-16 13:21:29"));
+app.get("/", (req,res)=>res.send("SkillSphereAI Server Running - 2025-08-16 "));
 app.listen(3000, ()=>console.log("Server started on port 3000"));
